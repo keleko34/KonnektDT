80e6f055c025dc545b34553885a5b05b0aff3bb4
keleko34 <gregory.guidero@gmail.com>
2017-01-16 18:25:50 +0100

    intial commit

---
+++ KObservable/KObservable.js
@@ -0,0 +1,22 @@
+define([],function(){
+  function CreateKObservable()
+  {
+    var KObservable = {},
+        events = {
+          "set":[],
+          "postset":[],
+          "add":[],
+          "postadd":[],
+
+        },
+        eventObject
+
+
+
+
+
+
+    return KObservable;
+  }
+  return CreateKObservable;
+}
